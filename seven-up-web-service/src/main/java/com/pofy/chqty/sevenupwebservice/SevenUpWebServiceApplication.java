7979c42ed397b6608564b81e265ecaf25af5be3f
POFY <pofy_jerry@hotmail.com>
2018-05-19 15:37:30 +0800

    project init
    Signed-off-by: POFY <pofy_jerry@hotmail.com>

---
+++ seven-up-web-service/src/main/java/com/pofy/chqty/sevenupwebservice/SevenUpWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.pofy.chqty.sevenupwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenUpWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SevenUpWebServiceApplication.class, args);
+    }
+}
---
+++ seven-up-web-service/src/test/java/com/pofy/chqty/sevenupwebservice/SevenUpWebServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pofy.chqty.sevenupwebservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SevenUpWebServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
