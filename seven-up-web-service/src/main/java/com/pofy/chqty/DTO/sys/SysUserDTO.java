a0ea81b4c0b8a50f29d93f7409a394e4ae4eb2b7
POFY <pofy_jerry@hotmail.com>
2018-05-21 01:08:19 +0800

    update controller return type
    Signed-off-by: POFY <pofy_jerry@hotmail.com>

---
+++ seven-up-web-service/src/main/java/com/pofy/chqty/DTO/sys/SysUserDTO.java
@@ -0,0 +1,38 @@
+package com.pofy.chqty.DTO.sys;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * function - 系统用户表对应实体
+ *
+ * @author POFY
+ * @version 1.0
+ * @date 2018-05-20
+ **/
+@Data
+public class SysUserDTO {
+
+    private String id;
+
+    private String userName;
+
+    private String userPassword;
+
+    private String randomCode;
+
+    private String userStatus;
+
+    private String userMobile;
+
+    private Date createdDate;
+
+    private String createdBy;
+
+    private Date updatedDate;
+
+    private String updatedBy;
+
+    private Integer roleId;
+}
--- seven-up-web-service/src/main/java/com/pofy/chqty/controller/sys/SysUserController.java
+++ seven-up-web-service/src/main/java/com/pofy/chqty/controller/sys/SysUserController.java
@@ -1,12 +1,16 @@
 package com.pofy.chqty.controller.sys;
 
+import com.pofy.chqty.DTO.sys.SysUserDTO;
 import com.pofy.chqty.entity.sys.SysUser;
 import com.pofy.chqty.service.sys.SysUserService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -23,11 +27,27 @@ public class SysUserController {
     @Autowired
     SysUserService sysUserService;
 
-    @RequestMapping(value = "users",method = RequestMethod.GET)
-    public List<SysUser> getSysUser(){
+    @RequestMapping(value = "allUsers",method = RequestMethod.GET)
+    public List<SysUserDTO> getAllSysUser(){
         SysUser sysUser = new SysUser();
-        List<SysUser> result = sysUserService.list(sysUser);
-        return result;
+        List<SysUser> sysUserList = sysUserService.list(sysUser);
+        List<SysUserDTO> resultList = new ArrayList<>();
+        for(SysUser user:sysUserList){
+            SysUserDTO tempDTO = new SysUserDTO();
+            BeanUtils.copyProperties(user,tempDTO);
+            resultList.add(tempDTO);
+        }
+        return resultList;
+    }
+
+    @RequestMapping(value = "user",method = RequestMethod.POST,consumes = "application/json")
+    public SysUserDTO getSysUser(@RequestBody SysUserDTO sysUserDTO){
+        SysUser sysUser = new SysUser();
+        BeanUtils.copyProperties(sysUserDTO,sysUser);
+        SysUser result = sysUserService.get(sysUser);
+        SysUserDTO resultDTO = new SysUserDTO();
+        BeanUtils.copyProperties(result,resultDTO);
+        return resultDTO;
     }
 
 }
