048fdfe3c59697664d2920735743c910d60a8c0b
POFY <pofy_jerry@hotmail.com>
2018-06-03 20:59:47 +0800

    去除log4j2，改用SpringBoot自带的logback，新增logback配置文件，删除log4j2配置文件。
    Signed-off-by: POFY <pofy_jerry@hotmail.com>

--- seven-up-web-service/src/main/java/com/pofy/chqty/RunService.java
+++ seven-up-web-service/src/main/java/com/pofy/chqty/RunService.java
@@ -2,10 +2,8 @@ package com.pofy.chqty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.pofy.chqty"})
 @tk.mybatis.spring.annotation.MapperScan("com.pofy.chqty.mapper")
 public class RunService {
 
