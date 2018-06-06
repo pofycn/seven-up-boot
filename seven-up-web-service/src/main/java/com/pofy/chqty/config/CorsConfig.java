7bbeb989dc3e53086686310045d2018088e8c7ca
陈宁 <chenn001@cmrh.com>
2018-06-06 17:03:10 +0800

    添加跨域配置

---
+++ seven-up-web-service/src/main/java/com/pofy/chqty/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.pofy.chqty.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * function - 跨域配置
+ *
+ * @author POFY
+ * @version 1.0
+ * @date 2018/06/06
+ */
+@EnableWebMvc
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("*")
+                .maxAge(3600);
+    }
+
+}
