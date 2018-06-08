2362678f5d37e548ea944555d30b10b3bdfff89b
POFY <pofy_jerry@hotmail.com>
2018-06-08 09:43:12 +0800

    开启对定时任务的支持

--- seven-up-web-service/src/main/java/com/pofy/chqty/RunService.java
+++ seven-up-web-service/src/main/java/com/pofy/chqty/RunService.java
@@ -2,8 +2,10 @@ package com.pofy.chqty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @tk.mybatis.spring.annotation.MapperScan("com.pofy.chqty.mapper")
 public class RunService {
 
